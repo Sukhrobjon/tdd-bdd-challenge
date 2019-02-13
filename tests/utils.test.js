3a434abb8e70b4d9303998dcc09ff5da8bee8e1e
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-02-13 15:09:33 -0800

    [finished] required tests

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -20,9 +20,17 @@ it("should say hello", function() {
 // Level 1 Challenges
 // ========================================================
 
-it("should return the area of a 5 by 6 rectangle");
+it("should return the area of a 5 by 6 rectangle", function() {
+  const area = utils.area(5, 6);
+  expect(area).to.be.a('Number');
+  expect(area).to.be.equal(5*6)
+});
 
-it("should return the are of a circle of radius 5");
+it("should return the are of a circle of radius 5", function(){
+  const areaCircle = utils.circleArea(5);
+  expect(areaCircle).to.be.a('Number');
+  expect(areaCircle).to.be.equal(Math.PI * 5 * 5)
+});
 
 // ========================================================
 // Level 2 Challenges
@@ -32,15 +40,47 @@ it("should return the are of a circle of radius 5");
 // tests eventually.
 // ========================================================
 
-it("Should create a new (object) Item with name and price");
+it("Should create a new (object) Item with name and price", function() {
+  const Item = {name: "Car", price: "25"};
+  expect(Item).to.be.a("Object");
+  expect(Item.name).to.equal('Car');
+  expect(Item.price).to.equal('25');
+});
 
-it("Should return an array containing all items in cart");
+it("Should return an array containing all items in cart", function() {
+  const Cart = [1, 2, 3]
+  expect(Cart).to.be.a("Array");
+  expect(Cart).to.have.lengthOf(3);
+  expect(Cart).to.have.members([3, 2, 1])
+});
 
-it("Should add a new item to the shopping cart");
 
-it("Should return the number of items in the cart");
+it("Should add a new item to the shopping cart", function() {
+  const Cart = [1, 2, 3]
+  expect(Cart).to.be.a("Array");
+  expect(Cart).to.have.lengthOf(Cart.length);
+  oldLength = Cart.length
+  Cart.push(4)
+  expect(Cart).to.have.lengthOf(oldLength + 1);
+  
+});
 
-it("Should remove items from cart");
+it("Should return the number of items in the cart", function() {
+  const Cart = [1, 2, 3]
+  expect(Cart).to.be.a("Array");
+  expect(Cart).to.have.lengthOf(Cart.length);
+});
+
+it("Should remove items from cart", function() {
+  const Cart = [1, 2, 3, 4]
+  expect(Cart).to.be.a("Array");
+  expect(Cart).to.have.lengthOf(Cart.length);
+  oldLength = Cart.length
+  for(let i = 0; i < 2; i++){
+    Cart.pop()
+  }
+  expect(Cart).to.have.lengthOf(oldLength - 2);
+});
 
 // ========================================================
 // Stretch Challenges
